dba55190a4538345f5f7b2075983ecd454670838
Mrhalix <aminaleahmad@yahoo.com>
2016-02-05 02:41:26 +0330

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -12,11 +12,12 @@ local action = function(msg)
 
 	local input = msg.text:input()
 	local amin = "*Mrhalix*"
+	local username = "@" msg.from.username
 	--local file = media/sticker.webp
 	if input then
 		sendMessage(msg.chat.id, latcyr(amin), use_markdown)
 		sendReply(msg, msg.from.first_name)
-		sendReply(msg, "@"),msg.from.username)
+		sendReply(msg, , ..username..)
 		sendReply(msg, msg.from.id)
 		--sendSticker = function(chat_id, media/sticker.webp , reply_to_message_id)
 	else

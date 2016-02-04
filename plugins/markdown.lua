20578f5c0009431e41e9bc21e3fee809c23f2cb6
Mrhalix <aminaleahmad@yahoo.com>
2016-02-05 01:23:21 +0330

    Update markdown.lua

--- plugins/markdown.lua
+++ plugins/markdown.lua
@@ -2,13 +2,14 @@ local command = 'Markdown'
 local doc = 'Markdown'
 
 local triggers = {
-	'^/!/inline'
+	'^/!/inline markdown'
 }
 
 local action = function(msg)
     local input =URL.escape(msg.text:input())
-    local text = "["..inline_block('بولد', '*'..input..'*')..', '
+    local text = "["..inline_block('Bold', '*'..input..'*')..', '
     text = text .. inline_block('Italic', '_'..input..'_')..', '
+    text = text .. inline_block('Link', '_'..input..'_')..', '
     text = text .. inline_block('Code', '```'..input..'```')..']'
     sendInline(msg.id, text)
 end

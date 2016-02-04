b3f720db211917e840e29dc3ff218b850300201b
Mrhalix <aminaleahmad@yahoo.com>
2016-02-05 01:26:35 +0330

    Update markdown.lua

--- plugins/markdown.lua
+++ plugins/markdown.lua
@@ -2,14 +2,16 @@ local command = 'Markdown'
 local doc = 'Markdown'
 
 local triggers = {
-	'^/!/inline markdown'
+	'^/!/inline'
 }
 
 local action = function(msg)
+    
     local input =URL.escape(msg.text:input())
     local text = "["..inline_block('Bold', '*'..input..'*')..', '
     text = text .. inline_block('Italic', '_'..input..'_')..', '
     text = text .. inline_block('Link', '_'..input..'_')..', '
+    text = text .. inline_block('Amin', '_'Mrhalix'_')..', '
     text = text .. inline_block('Code', '```'..input..'```')..']'
     sendInline(msg.id, text)
 end

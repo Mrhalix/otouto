6e182e1ae9379f99e06ca197381fad5f724a3cad
Mrhalix <aminaleahmad@yahoo.com>
2016-02-05 02:04:14 +0330

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -14,7 +14,7 @@ local action = function(msg)
 	local amin = "*MRhalix*"
 
 	if input then
-		sendMessage(msg.chat.id, latcyr(amin),use_markdown)
+		sendMessage(msg.chat.id, latcyr(amin), use_markdown)
 	else
 		sendMessage(msg.chat.id, doc, true, msg.message_id, true)
 	end
